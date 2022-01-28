4e6912c9107bc7c4a65e9c22cae4f67dbb3aa201
Ayres Major <98587842+ayresmajor@users.noreply.github.com>
2022-01-28 11:28:39 +0000

    Initial commit

---
+++ Exercícios/ex004/sript.js
@@ -0,0 +1,9 @@
+function clicar(){
+    let button = document.getElementById("botão")
+    button.style.backgroundColor = 'blue'
+    button.innerText = 'Clicou'
+}
+function sair(){
+    let button = document.getElementById("botão")
+    button.style.backgroundColor = 'red'
+}
